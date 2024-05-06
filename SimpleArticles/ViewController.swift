c751bf4ba43e415c78c866f6540383d77c7a5534
Will Saults <will@saults.io>
2024-05-06 18:14:09 -0500

    Initial Commit

---
+++ SimpleArticles/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleArticles
+//
+//  Created by Will Saults on 5/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
